b2e8b8aafcf700c202d118cbed92459086d7dff7
Zach <briggszj@gmail.com>
2013-12-30 21:07:46 -0600

    Indicate if a ship is docked by color

--- app/js/controllers/ships_ctrl.js
+++ app/js/controllers/ships_ctrl.js
@@ -1,7 +1,17 @@
-angular.module("app").controller("ShipsCtrl", function($scope) {
+angular.module("app").controller("shipsCtrl", function($scope, ships) {
   $scope.tutorial = { instructions: "Select Ship" };
-  $scope.ships = [{
-    name: "Patrol Boat",
-    hull: ["o", "o"],
-  }];
+  $scope.ships = ships;
+  $scope.handleShip = function(ship) {
+    ship.location = "transit";
+    $scope.tutorial.instructions = "Place " + ship.name;
+    console.log(ship.name);
+  };
+
+  $scope.docked = function(ship) {
+    if (ship.location === "docked") {
+      return "ship";
+    } else {
+      return "";
+    }
+  };
 });
---
+++ app/js/services/ships.js
@@ -0,0 +1,7 @@
+angular.module("app").factory("ships", function() {
+ return [{
+    name: "Patrol Boat",
+    hull: ["o", "o"],
+    location: "docked"
+  }];
+});
